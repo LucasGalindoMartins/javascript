abe69c30480f0b22d0a09be19a63fd70f093705d
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2024-09-19 13:13:26 -0300

    Primeira parte sobre condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 77
+console.log(`A velocida(de do seu carro é ${vel}km/h`)
+
+if(vel > 60) //Condição simples
+{
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!!")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
