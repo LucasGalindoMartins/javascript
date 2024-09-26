81199ab83544b35415ee432c096555a77645ca5a
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2024-09-26 14:59:20 -0300

    repetições parte 2

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++)
+{
+    console.log(c)
+}
+console.log('FIM!!!')
+
+
+// for(var c = 1; c <= 5; c++)
+// {
+//     console.log(`Contagem regressiva...${c}`)
+// }
+
+
+// var c = 1
+
+// while(c <= 5)
+// {
+//     console.log(`Contagem regressiva...${c}`)
+//     c++
+// }
