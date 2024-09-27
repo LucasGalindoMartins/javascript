8eabe1108d057929232da57b9f17ee6dc0faebae
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2024-09-27 14:13:34 -0300

    exercício de passos concluido

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,1 +1,41 @@
-alert('Olá')
+function contar()
+{
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var numi = Number(inicio.value)
+    var numf = Number(fim.value)
+    var nump = Number(passo.value)
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 || numi > numf)
+    {
+        res.innerHTML = "IMPOSSÍVEL CONTAR!!"
+    }
+
+    else if(nump == 0)
+    {
+        window.alert("Passo inválido!! Considerando PASSO 1")
+        nump = 1
+
+        res.innerHTML = `Contando: <br>`
+
+        for(var c = numi; c <= numf; c = c + nump)
+        {
+            res.innerHTML += `${c} &#x1F449;`
+        }
+
+        res.innerHTML += `&#x1F3C1;`
+    }
+
+    else if(nump != 0)
+    {
+        res.innerHTML = `Contando: <br>`
+
+        for(var c = numi; c <= numf; c = c + nump)
+        {
+            res.innerHTML += `${c} &#x1F449;`
+        }
+
+        res.innerHTML += `&#x1F3C1;`
+    }
+}
