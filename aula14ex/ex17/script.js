cab56b3623415ac4dd1108271469375bf370a0e2
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2024-09-30 20:40:26 -0300

    ex tabuada

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function gerar()
+{
+    var num = document.getElementById('inum')
+    var val = Number(num.value)
+
+    if(num.value.length == 0)
+    {
+        window.alert("Por favor, digite um número!!")
+    }
+
+    else
+    {
+        tabuada.innerHTML = `SUA TABUADA: <br>`
+
+        for(var c = 1; c <=10; c++)
+        {
+            var mul = val * c
+            tabuada.innerHTML += `${val}x${c} = ${mul}<br>`
+        }
+    }
+}
