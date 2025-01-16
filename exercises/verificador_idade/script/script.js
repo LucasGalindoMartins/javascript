12ba4aebd7d9b372748af7b41feeb78af6b2e3f0
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-01-16 13:52:58 -0300

    verificador de idade refeito

--- exercises/verificador_idade/script/script.js
+++ exercises/verificador_idade/script/script.js
@@ -4,6 +4,7 @@ function verificar()
     var ano = data.getFullYear()
     var fano = document.getElementById('itxtano')
     var res = document.querySelector('div#res')
+    var img = document.getElementById('img')
 
     if(fano.value.length == 0 || Number(fano.value) > ano)
     {
@@ -13,5 +14,50 @@ function verificar()
     {
         var fsex = document.getElementsByName('sex')
         var idade = ano - Number(fano.value)
+        var gen = ''
+
+        if(fsex[0].checked)
+        {
+            gen = 'Homem' 
+            if(idade >= 0 && idade < 15)
+            {
+                img.src= "img/crianca_mas.png"
+            }
+            else if (idade < 21)
+            {
+                img.src= "img/jovem_mas.png"
+            }
+            else if (idade < 60)
+            {
+                img.src= "img/adulto.png"
+            }
+            else
+            {
+                img.src= "img/idoso.png"
+            }
+        }
+        else if (fsex[1].checked)
+        {
+            gen = 'Mulher'
+            if(idade >= 0 && idade < 15)
+                {
+                    img.src= "img/crianca_fem.png"
+                }
+                else if (idade < 21)
+                {
+                    img.src= "img/jovem_fem.png"
+                }
+                else if (idade < 60)
+                {
+                    img.src= "img/adulta.png"
+                }
+                else
+                {
+                    img.src= "img/idosa.png"
+                }
+        }
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img) 
+        
     }
 }
